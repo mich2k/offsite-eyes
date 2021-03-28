1ff578d8bf1d2585f7c33509ac9f2dd065673f88
mich2k <miky3848@gmail.com>
2021-03-28 19:40:01 +0200

    init tutorials (activity+class) + stringfix calcs

---
+++ app/src/main/java/it/ooproject/offsiteeyes/MainTutorials.java
@@ -0,0 +1,14 @@
+package it.ooproject.offsiteeyes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainTutorials extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tutorials);
+    }
+}
